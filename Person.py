b178e60fbf5287114a6f4740771e2085b8e4f283
Clemens <clemens@herfarth.com>
2019-02-23 18:21:42 +0100

    Bug fixes, full screen enabled

--- Person.py
+++ Person.py
@@ -1,14 +1,16 @@
 import sys, pygame
+from random import randint
 global global_move
 
 class Person():
 	def __init__(self, MovementArray, MovementUnit, screen, MovementX, MovementY, background, designfolder):
-		self.design = pygame.image.load(self.designfolder+"running2right.png").convert_alpha()
-		self.look = "running2right"
+		self.designfolder = designfolder+"/"
+		self.design = pygame.image.load(self.designfolder+"r1.png").convert_alpha()
+		self.look = "r1"
 		self.movementArray = MovementArray
 		self.movementUnit = MovementUnit
-		self.movementX = MovementX
-		self.movementY = MovementY
+		self.movementX = int(MovementX)
+		self.movementY = int(MovementY)
 		self.rect = self.design.get_rect()
 		self.rect.right = 0
 		self.rect.bottom = 0
@@ -20,13 +22,16 @@ class Person():
 		self.in_jump = True
 		self.in_move = True
 		self.global_move = 0
-		self.Length_Move = 0.6
-		self.designfolder = designfolder+"/"
+		self.Length_Move = 0.2
 
 	def show(self):
 		if self.in_move != True and self.in_jump != True:
 			self.look = "standing"
 			self.design = pygame.image.load(self.designfolder+"standing.png")
+		try:
+			self.design = pygame.image.load(self.designfolder+self.look+".png")
+		except:
+			None
 		oldx = self.rect.right
 		oldy = self.rect.bottom
 		self.physics()
@@ -39,6 +44,8 @@ class Person():
 	def physics(self):
 		try:
 			assert(self.movementArray[self.movementY+1][self.movementX].movable == True or (self.in_jump == True and self.velocity < -1))
+			if self.movementArray[self.movementY-1][self.movementX].movable != True:
+				self.velocity = 0
 			if self.velocity == 0:
 				self.velocity += 5
 			self.velocity = self.velocity + 20
@@ -78,17 +85,17 @@ class Player(Person):
 		self.in_jump = True
 		self.in_move = False
 		self.global_move = 0
-		self.Length_Move = 0.6
+		self.Length_Move = 0.4
 
 
 
-	def move(self, direction, to_move):
+	def move(self, direction):
 		to_return = 0
 		self.in_move = True
 		#set values for speed depending on direction
-		SPEED = 40
+		SPEED = 20
 		if self.in_jump:
-			SPEED += -10
+			SPEED += 5
 		if direction == "right":
 			self.speed = SPEED
 		elif direction == "left":
@@ -113,7 +120,7 @@ class Player(Person):
 					self.design = pygame.image.load(self.designfolder+"running1left.png")
 		#check if move is allowed (if movable == True)
 		try:
-			self.movementUnit = self.movementArray[self.movementY-20][self.movementX]
+			self.movementUnit = self.movementArray[self.movementY][self.movementX]
 			if direction == "right":
 				if self.movementUnit.x - self.global_move > 500:
 					to_return = SPEED
@@ -124,7 +131,7 @@ class Player(Person):
 			#if not, make player go back
 			self.movementX += -self.speed
 			if direction == "right":
-				if self.movementUnit.x - (self.global_move - SPEED) > 500:
+				if self.movementUnit.x - (self.global_move) > 500:
 					to_return = 0
 					self.global_move += -SPEED * self.Length_Move
 			self.movementUnit = self.movementArray[self.movementY][self.movementX]
@@ -148,16 +155,30 @@ class Enemy(Person):
 
 	def __init__(self, MovementArray, MovementUnit, screen, MovementX, MovementY, background, designfolder):
 		super().__init__(MovementArray, MovementUnit, screen, MovementX, MovementY, background, designfolder)
-		self.direction = randint(0,11) % 2
+		"""self.direction = randint(0,11) % 2
 		if self.direction == 0:
-			self.direction = -1
+			self.direction = -1"""
+		self.direction = 1
+		print(self.direction)
 		self.distance = 0
-		self.speed = 35
+		self.speed = 15
 
 	def walk(self):
+		self.in_move = True
 		to_move = self.speed * self.direction
 		self.movementX += to_move
+		self.movementX = int(self.movementX)
 		self.distance += self.speed
+		if self.direction == 1:
+			if self.look == "r1":
+				self.look = "r2"
+			else:
+				self.look = "r1"
+		else:
+			if self.look == "l1":
+				self.look = "l2"
+			else:
+				self.look = "l1"
 		try:
 			self.movementUnit = self.movementArray[self.movementY][self.movementX]
 			assert(self.movementUnit.movable == True)
@@ -168,3 +189,6 @@ class Enemy(Person):
 		if self.distance > 300:
 			self.distance = 0
 			self.direction = self.direction * -1
+
+		self.physics()
+		self.show()
--- dungeon.py
+++ dungeon.py
@@ -1,25 +1,30 @@
 import sys, pygame
 from Field import Block, Movement_Unit
-from Player import Player
+from Person import *
 from time import *
+from pygame.locals import *
 pygame.init()
 pygame.key.set_repeat(10, 10)
 clock = pygame.time.Clock()
 
 size = width, height = 1000, 700
 to_show = []
-to_move = []
+people = []
 speed = [5, 9]
 black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 background = pygame.image.load("background.png").convert_alpha()
 
 #Define Length and Height of Movement_Units
-Length_Move = 0.6
+Length_Move = 0.4
 Height_Move = 0.4
 #Define Length and Height of Blocks
-LENGTH = 84
-HEIGHT = 84
+LENGTH = 42
+HEIGHT = 42
+assert(int(LENGTH/Length_Move)== LENGTH/Length_Move)
+assert(int(HEIGHT/Height_Move)== HEIGHT/Height_Move)
+assert(int(width/Length_Move)== width/Length_Move)
+assert(int(height/Height_Move)== height/Height_Move)
 
 
 def load_level(level):
@@ -53,25 +58,42 @@ def create_wall(x, y, plan):
 			for i in range(int((y*HEIGHT)/Height_Move),int(((y+1)*HEIGHT)/Height_Move)):
 				plan[i][q].setMovementStatus(False)
 
+def spider(MovementArray, MovementUnit, screen, MovementX, MovementY, background):
+	spider = Enemy(MovementArray, MovementUnit, screen, MovementX, MovementY, background, "Spider")
+	return spider
+
 
 def init_block(twoD_Array):
 	#Create two_dimensional Array for all Movement_Units
+	pygame.font.init()
+	myfont = pygame.font.SysFont('Comic Sans MS', 40)
+	completed = 0
+	to_be_completed = len(twoD_Array)*HEIGHT/Height_Move*len(twoD_Array[0])*LENGTH/Length_Move
 	plan = []
 	for y in range(int(len(twoD_Array)*HEIGHT/Height_Move)):
 		layer = []
 		for x in range(int(len(twoD_Array[0])*LENGTH/Length_Move)):
 			unit = Movement_Unit(x*Length_Move, y*Height_Move)
 			layer.append(unit)
-			to_move.append(unit)
+			completed += 1
 		plan.append(layer)
+		#textsurface = myfont.render(str(int(completed/to_be_completed*100))+"%", False, (100,100,255))
+		print(str(int((completed/to_be_completed)*100))+"%")
+		#screen.blit(textsurface, (0,0))
+		#pygame.display.flip()
 	#Iterate through input
 	for y in range(len(twoD_Array)):
 		for x in range(len(twoD_Array[y])):
 			#Create Dungeon_Wall for input "_"
 			if twoD_Array[y][x] == "_":
 				create_wall(x, y, plan)
-			"""elif twoD_Array[y][x] == "s":"""
-
+	#MovementArray, MovementUnit, screen, MovementX, MovementY, background, designfolder
+	for y in range(len(twoD_Array)):
+		for x in range(len(twoD_Array[y])):
+			"""if twoD_Array[y][x] == "s":
+				movementUnit = plan[int((y-3)*HEIGHT/Height_Move)][int((x+2)*LENGTH/Length_Move)]
+				enemy = spider(plan, movementUnit, screen, x * Length_Move, y * Height_Move, background)
+				people.append(enemy)"""
 
 	return plan
 
@@ -84,16 +106,18 @@ current_key = None
 
 while True:
 	should_move = 0
-	clock.tick(60)
+	clock.tick(20)
 	screen.blit(background, (0, 0))
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 	keys = pygame.key.get_pressed()
+	if keys[pygame.K_a]:
+		pygame.display.set_mode(size)
 	if keys[pygame.K_RIGHT]:
-		should_move = player.move("right", to_move)
+		should_move = player.move("right")
 	elif keys[pygame.K_LEFT]:
-		should_move = player.move("left", to_move)
+		should_move = player.move("left")
 	if keys[pygame.K_UP] or keys[pygame.K_SPACE]:
 		player.jump()
 	for item in to_show:
@@ -102,4 +126,7 @@ while True:
 		except:
 			None
 		item.show()
+	for person in people:
+		person.walk()
 	pygame.display.flip()
+	sleep(0.01)
